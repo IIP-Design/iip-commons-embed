88e544aa743cd11102859791f30cba9e409875d0
mrewers <mrewers@users.noreply.github.com>
2018-10-18 15:51:28 -0400

    Structure app

---
+++ admin/js/src/containers/EmbedConverter.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import EmbedCodeInput from '../components/EmbedCodeInput';
+import ShortcodeOutput from '../components/ShortcodeOutput';
+import ConvertButton from '../components/ConvertButton';
+import ClearButton from '../components/ClearButton';
+
+import { parseEmbedCode } from '../utils/article-converter';
+
+class EmbedConverter extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      input: 'Paste your embed code here',
+      output: '[]'
+    };
+
+    this.handleChange = this.handleChange.bind( this );
+    this.parseInput = this.parseInput.bind( this );
+    this.resetFields = this.resetFields.bind( this );
+  }
+
+  handleChange( e ) {
+    this.setState( {
+      input: e.target.value
+    } );
+  }
+
+  parseInput( input ) {
+    const shortcode = parseEmbedCode( input );
+
+    this.setState( {
+      output: shortcode
+    } );
+  }
+
+  resetFields() {
+    this.setState( {
+      input: 'Paste your embed code here',
+      output: '[]'
+    } );
+  }
+
+  render() {
+    const {
+      input, output
+    } = this.state;
+
+    return (
+      <div className="iipce-embed-converter">
+        <EmbedCodeInput callback={ this.handleChange } input={ input } />
+        <div className="iipce-embed-button-container">
+          <ConvertButton callback={ this.parseInput } input={ input } />
+          <ClearButton callback={ this.resetFields } />
+        </div>
+        <ShortcodeOutput output={ output } />
+      </div>
+    );
+  }
+}
+
+export default EmbedConverter;
